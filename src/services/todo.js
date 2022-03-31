25d15ab2436d988a701dac152172566e1dec96ff
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-31 16:53:56 -0700

    complete updating on click

--- src/services/todo.js
+++ src/services/todo.js
@@ -6,6 +6,18 @@ export async function getTodos() {
 }
 
 export async function updateTodos(todo) {
-  const resp = await client.from('todos').insert(todo);
+  const resp = await client.from('todos').insert({ todo: todo });
   return checkError(resp);
+}
+
+export async function updateStatus(id) {
+  const tf = await client.from('todos').select('complete').match({ id }).single();
+
+  if (tf.data.complete === false) {
+    const resp = await client.from('todos').update({ complete: true }).match({ id });
+    return checkError(resp);
+  } else {
+    const resp = await client.from('todos').update({ complete: false }).match({ id });
+    return checkError(resp);
+  }
 }
--- src/views/Todo.js
+++ src/views/Todo.js
@@ -1,27 +1,62 @@
 import { useEffect, useState } from 'react';
-import { getTodos } from '../services/todo';
+import { getTodos, updateStatus, updateTodos } from '../services/todo';
+import styled from 'styled-components';
 
 function Todo() {
   const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  //this state adds a layer between the typing and the click
+  //so that it doesn't update every time you type something in the input
+  const [sendTodo, setSendTodo] = useState('');
+  //making state to update false to true
+  const [complete, setComplete] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       const data = await getTodos();
       setTodos(data);
     };
     fetchData();
-  }, []);
+  }, [sendTodo]);
 
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    await updateTodos(newTodo);
+    //updating the useEffect to reload the page
+    setSendTodo(newTodo);
+    setNewTodo('');
+  };
+
+  // const handleComplete = async (e) => {
+  //   setComplete(!complete);
+  //   await updateStatus(e.target.id);
+  // };
   return (
     <div>
       {todos.map((todo) => (
-        <p key={todo.id}>{todo.todo}</p>
+        <StyledP
+          className={complete ? 'active' : ''}
+          key={todo.id}
+          onClick={async () => updateStatus(todo.id)}
+        >
+          {todo.todo}
+        </StyledP>
       ))}
       <form action="">
-        <input type="text" placeholder="Todo's" />
-        <button>Add Todo</button>
+        <input
+          type="text"
+          value={newTodo}
+          placeholder="Todo's"
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={handleUpdate}>Add Todo</button>
       </form>
     </div>
   );
 }
 
+const StyledP = styled.p`
+  .active {
+    text-decoration: strike;
+  }
+`;
 export default Todo;

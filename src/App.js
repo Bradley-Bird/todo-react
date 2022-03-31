e86092062fe6d0041c7b53954803b72d43ea3dbd
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-30 17:01:50 -0700

    added getUser to currentUsers state to keep state from losing auth status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import { useState } from 'react';
 import Home from './views/Home';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Todo from './views/Todo';
+import { getUser } from './services/auth';
 
 function App() {
+  const user = getUser();
+  const [currentUser, setCurrentUser] = useState(user);
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/todo">{currentUser ? <Todo /> : <Redirect to="/" />}</Route>
+          <Route path="/">
+            {currentUser ? <Redirect to="/todo" /> : <Home {...{ setCurrentUser }} />}
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,12 +3,12 @@ import { useHistory } from 'react-router-dom';
 import { signInUser, signupUser } from '../services/auth';
 import Auth from '../components/Authform';
 
-function Home() {
+function Home({ setCurrentUser }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [type, setType] = useState('sign-in');
-  const [currentUser, setCurrentUser] = useState(null);
+
   const history = useHistory();
 
   const handleSubmit = async (e) => {
@@ -17,6 +17,7 @@ function Home() {
       try {
         const resp = await signInUser(email, password);
         setCurrentUser(resp.email);
+        history.push('/todo');
       } catch (e) {
         setError(e.message);
       }
